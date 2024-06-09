0e47e575660cdc3d832c0e4d00a30ccc5fadef67
Orlando Verdin <git@orli.land>
2024-06-08 19:02:22 -0700

    fix problem with https request

--- src/utils/getIpData.jsx
+++ src/utils/getIpData.jsx
@@ -1,6 +1,6 @@
 async function getIpData(ip) {
   const url = `http://ip-api.com/json/${ip}?fields=58356`;
-  let response = await fetch(url);
+  let response = await fetch(url, { referrerPolicy: "unsafe-url" });
   try {
     let request = await response.json();
     return request;
